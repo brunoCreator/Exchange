a04036e1fb5511068febf400346866c4d0e16e60
brunoCreator <brunoCreator@users.noreply.github.com>
2020-08-11 03:30:34 -0300

    done

---
+++ js/main.js
@@ -0,0 +1,39 @@
+if(window.location.href.split("/").pop().includes("jogos")){
+    
+   var steam = document.getElementById("steam");
+var xbox = document.getElementById("xbox");
+var playstation = document.getElementById("playstation");
+
+steam.addEventListener('mouseover', opaco);
+xbox.addEventListener('mouseover', opaco);
+playstation.addEventListener('mouseover', opaco);
+
+steam.addEventListener('mouseout', normal);
+xbox.addEventListener('mouseout', normal);
+playstation.addEventListener('mouseout', normal);
+
+function opaco(){
+    this.style.opacity = "0.5";
+}
+
+function normal(){
+    this.style.opacity = "1";
+}
+}
+var telefone = document.getElementById("telefone");
+telefone.addEventListener("focusout", function(e) {
+
+      var numeros = ('' + telefone.value).replace(/\D/g, '').substr(0,11);
+      telefone.value = numeros.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");;
+})
+var email = document.getElementById("email");
+email.addEventListener("focusout", function(e) {
+
+      const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(re.test(email.value)){
+           
+}else{
+    alert("Invalido");
+}
+
+})
